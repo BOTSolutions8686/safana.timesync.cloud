1de05c07b7989fa12978ab6edd035cfb60cd50d5
Asad Zaman <AsadZaman177@gmail.com>
2023-12-16 22:15:10 +0500

    Asad commit

--- app/Models/Hrm/Attendance/Weekend.php
+++ app/Models/Hrm/Attendance/Weekend.php
@@ -3,13 +3,16 @@
 namespace App\Models\Hrm\Attendance;
 
 use App\Models\coreApp\Traits\Relationship\StatusRelationTrait;
+use App\Models\Traits\BranchTrait;
+use App\Models\Traits\CompanyTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class Weekend extends Model
 {
-    use HasFactory, StatusRelationTrait, LogsActivity;
+    
+    use HasFactory, StatusRelationTrait, LogsActivity, CompanyTrait,BranchTrait;
 
     protected $fillable = [
         'company_id', 'name', 'is_weekend', 'status_id'
